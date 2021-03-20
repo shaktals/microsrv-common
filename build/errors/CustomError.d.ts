ac70decd4bcea796c6837f3ee0154ac8f6b907a6
Talles Hentges <talles.hentges@gmail.com>
2021-03-20 20:10:27 -0300

    Remove build folder from git

--- build/errors/CustomError.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ErrorObject {
-    message: string;
-    field?: string;
-}
-export declare abstract class CustomError extends Error {
-    abstract statusCode: number;
-    constructor(message: string);
-    abstract serializeErrors(): ErrorObject[];
-}
--- build/middlewares/validateRequest.d.ts
+++
@@ -1,2 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const validateRequest: (req: Request, resp: Response, next: NextFunction) => void;
