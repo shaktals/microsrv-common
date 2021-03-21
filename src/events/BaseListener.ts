7d5aad42e8053074ae379ba7fd16f4c835de71d2
Talles Hentges <talles.hentges@gmail.com>
2021-03-21 11:15:32 -0300

    Fix up BaseListener exported class name to Listener

--- src/events/BaseListener.ts
+++ src/events/BaseListener.ts
@@ -6,7 +6,7 @@ interface Event {
   data: any
 }
 
-export default abstract class BaseListener<T extends Event> {
+export abstract class Listener<T extends Event> {
   abstract subject: T['subject']
   abstract queueGroupName: string
   abstract onMessage(data: T['data'], msg: Message): void
